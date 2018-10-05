bdedb3e94061deba3d0e8eabe03750dd406da280
edvinhakobyan <edvin1984edvin@gmail.com>
2018-10-05 18:49:37 +0400

    ww

--- C-Sharp-Lessons/Geometric_Figures/Program.cs
+++ C-Sharp-Lessons/Geometric_Figures/Program.cs
@@ -14,51 +14,112 @@ namespace Geometric_Figures
             Console.WriteLine("1: Rectangle");
             Console.WriteLine("2: Circle");
             Console.WriteLine("3: Rhombus");
-            Console.Write("Shape: ");
-            int number = TrueNumber(1, 4);
-            if (number == 1)
+            while (true)
             {
-                Console.WriteLine("Enter Length of Rectangle Sides: ");
-                Console.Write("A = ");
-                int A = TrueNumber(int.MinValue, int.MaxValue);
-                Console.Write("B = ");
-                int B = TrueNumber(int.MinValue, int.MaxValue);
-                Rectangle( A, B);
+                Console.Write("Shape: ");
+                int number = TrueNumber(1, 4);
+                if (number == 1)
+                {
+                    Console.WriteLine("Enter Length of Rectangle Sides: ");
+                    Console.Write("A = ");
+                    int A = TrueNumber(0, int.MaxValue);
+                    Console.Write("B = ");
+                    int B = TrueNumber(0, int.MaxValue);
+                    Rectangle(A, B);
+                }
+                else if (number == 2)
+                {
+                    Console.WriteLine("Enter Radius of Circle: ");
+                    Console.Write("R = ");
+                    int R = TrueNumber(0, int.MaxValue);
+                    Circle(R);
+                }
+                else
+                {
 
+                }
             }
-            else if (number == 2)
+        }
+
+        private static void Circle(int r)
+        {
+            Console.WriteLine();
+            int a = Console.WindowWidth / 3;
+            int b = Console.CursorTop + r + 5;
+
+
+            for (int i = 0; i < 2 * r + 1; i++)
             {
+                Console.WriteLine();
+                for (int j = 0; j < 20 * r; j++)
+                {
+                    if (j == a)
+                    {
 
+                    }
+                    int temp = ((a - j) * (a - j) + (b - i) * (b - i));
+                    if (temp == (r * r))
+                        Console.Write('*');
+                    else
+                        Console.Write(' ');
+                }
             }
-            else
-            {
-
-            }
-
 
         }
 
-        private static void Rectangle(int a, int b)
+        private static void Rectangle(int w, int h)
         {
-            for (int i = 1; i <= a; i++)
+            Console.WriteLine();
+            int A = w * h;
+            int min = w <= h ? w : h;
+            int max = min == w ? h : w;
+
+            w = max;
+            h = min;
+
+            int wScale = Console.WindowWidth / 3;
+            int hScale = wScale * min / max;
+
+            hScale = hScale > 2 ? hScale : 3;
+            hScale |= 1;
+
+            int x = Console.CursorLeft + 10;
+            int y = Console.CursorTop;
+
+            int wLength = max.ToString().Length;
+            int hLength = min.ToString().Length;
+            int ALength = $"A = {A}".Length;
+
+            for (int i = 1; i <= hScale; i++)
             {
-                for (int j = 1; j <= b; j++)
+                Console.Write(new string(' ', 10));
+                for (int j = 1; j <= wScale; j++)
                 {
-                    if (i == 1 || i == a)
-                        Console.Write('-');
+                    if (i == 1 || i == hScale)
+                    {
+                        if (j == 1 || j == wScale)
+                            Console.Write('+');
+                        else
+                            Console.Write('-');
+                    }
                     else
                     {
-                        Console.Write('|');
-                        Console.Write(new string(' ', b - 2));
-                        Console.Write('|');
-                        break;
+                        if (j == 1 || j == wScale)
+                            Console.Write('|');
+                        else
+                            Console.Write(' ');
                     }
-
-
                 }
                 Console.WriteLine();
             }
-            
+            Console.SetCursorPosition(x + wScale / 2 - wLength + 1, y - 1);
+            Console.Write(max);
+            Console.SetCursorPosition(x - hLength, y + hScale / 2);
+            Console.Write(min);
+            Console.SetCursorPosition(x + (wScale - ALength) / 2, y + hScale / 2);
+            Console.Write($"A = {A}");
+            Console.SetCursorPosition(0, y + hScale);
+            Console.WriteLine();
         }
 
         static int TrueNumber(int min, int max)
