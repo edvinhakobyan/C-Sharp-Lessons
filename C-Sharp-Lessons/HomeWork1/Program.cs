07322ad17be73e363074fe722020f8c489e9e8ac
Edvin <edvin-hakobyan@mail.ru>
2018-10-13 01:33:05 +0400

    add HomeWork1

---
+++ C-Sharp-Lessons/HomeWork1/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem26(TrueNumberFromConsole(0,ulong.MaxValue));
+        }
+
+        /*Խնդիր_16:
+          Հաշվել տրված բնական թվի թվանշանների գումարը, քանակը, արտադրյալը։*/
+        static void Problem16(ulong N)
+        {
+            ulong count = 0, sum = 0, mult = 1, dig = 0;
+
+            while (N > 0)
+            {
+                dig = N % 10;
+                sum += dig;
+                mult *= dig;
+                count++;
+                N /= 10;
+            }
+            Console.WriteLine($"count = {count}");
+            Console.WriteLine($"sum = {sum}");
+            Console.WriteLine($"mult = {mult}");
+        }
+
+
+        /*Խնդիր_17:
+          Գտնել տրված թվի կենտ թվանշաններից ամենափոքրը։*/
+        static void Problem17(ulong N)
+        {
+            ulong min = 9, t = 0;
+            while (N > 0)
+            {
+                t = N % 10;
+                if ((t & 1) == 1)
+                    min = t < min ? t : min;
+                N /= 10;
+            }
+            Console.WriteLine($"min = {min}");
+        }
+
+
+        /*Խնդիր_18:
+          Հաշվել տրված թվի 4-ից մեծ կենտ թվանշանների գումարը։*/
+        static void Problem18(ulong N)
+        {
+            ulong sum = 0, t = 0;
+            while (N > 0)
+            {
+                t = N % 10;
+                if ((t & 1) == 1 && t > 4)
+                    sum += t;
+                N /= 10;
+            }
+            Console.WriteLine($"sum = {sum}");
+        }
+
+
+        /*Խնդիր_19:
+          Հաշվել տրված թվի 7-ից փոքր զույգ թվանշանների արտադրյալը։*/
+        static void Problem19(ulong N)
+        {
+            ulong mult = 1, t = 0;
+            while (N > 0)
+            {
+                t = N % 10;
+                if ((t & 1) == 0 && t < 7)
+                    mult *= t;
+                N /= 10;
+            }
+            Console.WriteLine($"sum = {mult}");
+        }
+
+
+        /*Խնդիր_20:
+          Գտնել տրված թվի ամենամեծ և ամենափոքր թվանշանների տարբերության քառակուսին։*/
+        static void Problem20(ulong N)
+        {
+            ulong max = 0, min = 9, t = 0;
+            while (N > 0)
+            {
+                t = N % 10;
+                min = t < min ? t : min;
+                max = t > max ? t : max;
+                N /= 10;
+            }
+            Console.WriteLine($"(max - min)^2 = {(max - min) * (max - min)}");
+        }
+
+        /*Խնդիր_21:
+          Արտածել տրված թվի բոլոր բաժանարարները։
+          Խնդիր_22:
+          Հաշվել տրված թվի բոլոր բաժանարարների գումարը։*/
+
+        static void Problem21_22(ulong N)
+        {
+            ulong sum = N;
+            for (ulong i = 1; i < N / 2; i++)
+                if (N % i == 0)
+                {
+                    sum += i;
+                    Console.WriteLine(i);
+                }
+            Console.WriteLine(N);
+            Console.WriteLine("\n" + sum);
+        }
+
+        /*Խնդիր_24:
+         Պարզել, հանդիսանում է, արդյոք, տրված թիվը 3-ի աստիճան։*/
+        static void Problem24(ulong N)
+        {
+            ulong n = 0, N1 = N;
+            while (N > 1 && N % 3 == 0)
+            { N /= 3; n++;  }
+
+            if (N == 1)
+                Console.WriteLine($"{N1} = 3^{n}");
+            else
+                Console.WriteLine($"{N1} != 3^x");
+        }
+
+        /*Խնդիր_25:
+          Տրված n>0 ամբողջ թվի համար հաշվել 2-ի ամենամեծ աստիճանը, որը չի գերազանցում n-ը։*/
+        static void Problem25(ulong N)
+        {
+            ulong N1 = N, p = 0;
+            while( (N >>= 1) > 0)  ++p;
+            Console.WriteLine($"{N1} ~ 2^{p}");
+        }
+
+        /*Խնդիր_26:
+          Գտնել տրված բնական թվից մեծ և 2-ի աստիճան հանդիսացող ամենափոքր թիվը։*/
+        static void Problem26(ulong N)
+        {
+            ulong N1 = 1, p = 0;
+            while ((N1 <<= 1) < N) ++p;
+            Console.WriteLine($"{N} ~ 2^{p} = {N1}");
+        }
+
+        static ulong TrueNumberFromConsole(ulong min, ulong max)
+        {
+            ulong number;
+            while (!(ulong.TryParse(Console.ReadLine(), out number) && number >= min && number < max))
+                Console.WriteLine("Wrong Selection");
+            return number;
+        }
+    }
+}
---
+++ C-Sharp-Lessons/HomeWork1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HomeWork1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("HomeWork1")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("71217fa3-0433-4c57-bbb5-9ece04b364b6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
