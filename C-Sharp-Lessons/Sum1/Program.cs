ff8d8d61814f2422234f8cb52f3e29b6cf615d10
Edvin <edvin-hakobyan@mail.ru>
2018-10-12 16:40:29 +0400

    ADD

--- C-Sharp-Lessons/Sum1/Program.cs
+++ C-Sharp-Lessons/Sum1/Program.cs
@@ -7,30 +7,40 @@ using System.Threading.Tasks;
 namespace Sum1
 {
     class Program
-    {
-        // Console.WriteLine("1 + X/2 – 1*X^2/(2*4) + 1*3*X^3/(2*4*6) – 1*3*5*X^4/(2*4*6*8) + ... ");
+    {   
         static void Main(string[] args)
         {
-            Sum(10);
+            double tt = Sum(10);
         }
 
+        //                    0   1       2               3                   4
+        // Console.WriteLine("1 + X/2 – 1*X^2/(2*4) + 1*3*X^3/(2*4*6) – 1*3*5*X^4/(2*4*6*8) + ... ")
 
-
+        /// <summary>
+        /// RETURN SUM OF THIS ROW [1 + X/2 – 1*X^2/(2*4) + 1*3*X^3/(2*4*6) – 1*3*5*X^4/(2*4*6*8) + ... ]
+        /// </summary>
+        /// <param name="X"></param>
+        /// <returns></returns>
         static double Sum(ulong X)
         {
-            ulong N = 10;
+            ulong N = 15;
             double sum = 1;
+            Console.WriteLine(sum + '+');
             for (ulong i = 1; i <= N; i++)
             {
                 sum += Math.Pow(-1, i) * Numerator(i) * Math.Pow(X, i) / Denominator(i);
-                Console.Write("[{3}{1} * X^{0} / {2}] + ", i, Numerator(i), Denominator(i), Math.Pow(-1, i ^ 1) > 0 ? '+' : '-');
+                Console.WriteLine("[{3}{1} * X^{0} / {2}] + ", i, Numerator(i), Denominator(i), Math.Pow(-1, i ^ 1) > 0 ? '+' : '-');
             }
-            Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop);
+            //Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop-1);
             Console.WriteLine("= " + sum);
-
             return sum;
         }
 
+        /// <summary>
+        /// RETURN MEBER BY INDEX OF THIS ROW [1] [1] [1] [3] [3*5] [3*5*7] [3*5*7*9]...
+        /// </summary>
+        /// <param name="ind">INDEX OF MEMBER</param>
+        /// <returns></returns>
         static ulong Numerator(ulong ind)
         {
             if (ind < 3) return 1;
@@ -40,6 +50,11 @@ namespace Sum1
             return ret;
         }
 
+        /// <summary>
+        /// RETURN MEBER BY INDEX OF THIS ROW [1] [2] [2*4] [2*4*6] [2*4*6*8] ...
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
         static ulong Denominator(ulong n)
         {
             ulong ret = 1, j = 0;
