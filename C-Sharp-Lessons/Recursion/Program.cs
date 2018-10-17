dc5e04c9da4799ba6b88609088f2e9a8a5d47ad4
edvinhakobyan <edvin1984edvin@gmail.com>
2018-10-17 21:00:27 +0400

    qqq

--- C-Sharp-Lessons/Recursion/Program.cs
+++ C-Sharp-Lessons/Recursion/Program.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Numerics;
 
 namespace Recursion
 {
     class Program
     {
-        static void Main1(string[] args)
+        static void Main(string[] args)
         {
-            // for (BigInteger i = 0; i < 120; i++)
+            Console.SetCursorPosition(0, 50);
+            for (int i = 0; i < 100000000; i++)
+            {
+                DateTime t = DateTime.Now;
+                Factorial(5000*i);
+                Console.SetCursorPosition(i, 50-(DateTime.Now - t).Seconds);
+                Console.Write('*');
+            }
+            Console.ReadKey();
+        }
 
-            Console.WriteLine(Factorial(5000));
-
+        public static ulong recursion(ulong n, ulong i)
+        {
+            return (n == 0) ? i : recursion(n / 10, i * 10 + n % 10);
         }
 
         static BigInteger Factorial_Recursion(BigInteger n)
@@ -22,14 +32,6 @@ namespace Recursion
             if (n <= 1) return 1;
             return n * Factorial_Recursion(n - 1);
         }
-
-        /*static int SumDigit(ulong n)
-        {
-            if (n == 0) return 1;
-
-            return 1;
-        }*/
-
 
 
         static BigInteger Factorial(BigInteger n)
@@ -39,8 +41,7 @@ namespace Recursion
                 ret = BigInteger.Multiply(ret, i);
             return ret;
         }
-
-
+        
 
         // 1 1 2 3 5 8
         static ulong Fibonachi(ulong n)
