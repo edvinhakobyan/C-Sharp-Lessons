d25e7475e9a07d705769ab5883f93cb8cf40af12
edvinhakobyan <edvin1984edvin@gmail.com>
2018-10-17 10:44:04 +0400

    add

--- C-Sharp-Lessons/Recursion/Program.cs
+++ C-Sharp-Lessons/Recursion/Program.cs
@@ -11,14 +11,22 @@ namespace Recursion
     {
         static void Main(string[] args)
         {
-           // for (BigInteger i = 0; i < 120; i++)
-            
-            Console.WriteLine(Factorial(5000));
-            
+            // for (BigInteger i = 0; i < 120; i++)
+
+            write(5);
 
             Console.ReadKey();
         }
 
+
+        static BigInteger Factorial1(BigInteger n)
+        {
+            BigInteger ret = 1;
+            for (int i = 1; i <= n; i++)
+                ret = BigInteger.Multiply(ret, i);
+
+            return ret;
+        }
 
         static BigInteger Factorial(BigInteger n)
         {
@@ -31,20 +39,20 @@ namespace Recursion
         static ulong Fibonachi(ulong n)
         {
             if (n <= 1) return 1;
-            return Fibonachi(n-2) + Fibonachi(n-1);
+            return Fibonachi(n - 2) + Fibonachi(n - 1);
         }
 
 
         static void write(int num)
         {
-            if(num != 0)
+            if (num != 0)
             {
                 write(num - 1);
-                Console.WriteLine($"*{num}");
-                write(num - 1); 
+                Console.WriteLine($"{num}");
+                //bwrite(num - 1); 
             }
             else
-                Console.WriteLine(num); 
+                Console.WriteLine(num); //0 0 1 1 2 2 3 3 4 4 5 5
         }
     }
 }
