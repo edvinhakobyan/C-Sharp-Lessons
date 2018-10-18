7ddc52aec3a9d96f22867d9e69e2c8298bc35e55
edvinhakobyan <edvin1984edvin@gmail.com>
2018-10-18 16:42:26 +0400

    www

--- C-Sharp-Lessons/Recursion/Program.cs
+++ C-Sharp-Lessons/Recursion/Program.cs
@@ -11,16 +11,90 @@ namespace Recursion
     {
         static void Main(string[] args)
         {
-            Console.SetCursorPosition(0, 50);
-            for (int i = 0; i < 100000000; i++)
+            //Console.SetCursorPosition(0, 50);
+            //for (ulong i = 0; i < 100000000; i++)
+            //{
+            //    DateTime t = DateTime.Now;
+            //    Fibonachi(i);
+            //    Console.SetCursorPosition((int)i, 50-(DateTime.Now - t).Seconds);
+            //    Console.Write('*');
+            //}
+            //Console.ReadKey();
+            for (ulong i = 1; i < 50; i++)
             {
-                DateTime t = DateTime.Now;
-                Factorial(5000*i);
-                Console.SetCursorPosition(i, 50-(DateTime.Now - t).Seconds);
-                Console.Write('*');
+                Console.WriteLine(i + " " + 1.0*Fibonachi(i)/Fibonachi(i - 1));
             }
             Console.ReadKey();
+
         }
+
+        // 1 1 2 3 5 8
+        static ulong Fibonachi(ulong n)
+        {
+            if (n <= 1) return 1;
+            return Fibonachi(n - 2) + Fibonachi(n - 1);
+        }
+
+
+
+        //0 1 0 1 1 0 1 1 0 1
+        public static int recursion(int a, int b)
+        {
+            // Базовый случай 
+            if (a > b + 1)   return 0;
+            
+            // Базовый случай 
+            if (a == 0 || b == 0) return 1;
+            
+            // Шаг рекурсии / рекурсивное условие
+            return recursion(a, b - 1) + recursion(a - 1, b - 1);
+        }
+
+        public static String recursion1(int a, int b)
+        {
+            // основное условие задачи
+            if (a > b)
+            {
+                // Базовый случай
+                if (a == b)
+                {
+                    return a.ToString();
+                }
+                // Шаг рекурсии / рекурсивное условие
+                return a + " " + recursion(a - 1, b);
+            }
+            else
+            {
+                // Базовый случай
+                if (a == b)
+                {
+                    return a.ToString();
+                }
+                // Шаг рекурсии / рекурсивное условие
+                return  recursion(a, b-1) + " " + b;
+            }
+        }
+
+        public static void Convert_to_bynary1(int n, int m)
+        {
+            int temp = n % m;
+            if (n >= 2)
+                Convert_to_bynary1(n / m, m);
+            Console.Write(temp);
+        }
+
+        public static String recursion(int n)
+        {
+            // Базовый случай
+            if (n == 1)
+            {
+                return "1";
+            }
+            // Шаг рекурсии / рекурсивное условие
+            return recursion(n - 1) + " " + n;
+        }
+
+
 
         public static ulong recursion(ulong n, ulong i)
         {
@@ -43,12 +117,7 @@ namespace Recursion
         }
         
 
-        // 1 1 2 3 5 8
-        static ulong Fibonachi(ulong n)
-        {
-            if (n <= 1) return 1;
-            return Fibonachi(n - 2) + Fibonachi(n - 1);
-        }
+
 
     }
 }
