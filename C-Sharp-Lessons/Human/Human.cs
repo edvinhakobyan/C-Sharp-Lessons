0188942bcb8478d5d92c7bb450a9b642370000f1
edvinhakobyan <edvin1984edvin@gmail.com>
2018-11-08 14:25:03 +0400

    human

---
+++ C-Sharp-Lessons/Human/Human.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    class Human
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+        public double weight { get; set; }
+        public string gender { get; set; }
+
+        public Human()
+        {
+
+        }
+        public Human(string name):this()
+        {
+            this.name = name; 
+        }
+        public Human(string name, string surname) : this(name)
+        {
+            this.surname = surname;
+        }
+        public Human(string name, string surname, int age) : this(name, surname)
+        {
+            this.age = age;
+        }
+        public Human(string name, string surname, int age, double weight) : this(name, surname, age)
+        {
+            this.weight = weight;
+        }
+        public Human(string name, string surname, int age, double weight, string gender) : this(name, surname, age, weight)
+        {
+            this.gender = gender;
+        }
+    }
+}
+
+
---
+++ C-Sharp-Lessons/Human/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h = new Human();
+            Human h1 = new Human("Edvin");
+            Human h2 = new Human("Edvin", "Hakobyan");
+            Human h3 = new Human("Edvin", "Hakobyan", 33);
+            Human h4 = new Human("Edvin", "Hakobyan", 33, 77);
+            Human h5 = new Human("Edvin", "Hakobyan", 33, 77, "Man");
+        }
+    }
+}
