0b3cb7f64a0eb7fcfa340d4b20aab37d792e5cab
edvinhakobyan <edvin1984edvin@gmail.com>
2018-11-28 12:19:23 +0400

    rrr

---
+++ C-Sharp-Lessons/C-Sharp-Lessons/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lessons
+{
+    class Class1
+    {
+        public void method()
+        {
+            Program.chenge_a();
+        }
+    }
+}
--- C-Sharp-Lessons/C-Sharp-Lessons/Program.cs
+++ C-Sharp-Lessons/C-Sharp-Lessons/Program.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace C_Sharp_Lessons
 {
     class Program
     {
+        static int a;
         static void Main(string[] args)
         {
+            Class1 w = new Class1();
 
+            ThreadStart del = new ThreadStart(w.method);
+            Thread t = new Thread(del);
+            t.Start();
+        }
+
+        public static void chenge_a()
+        {
+            a++;
         }
     }
 }
