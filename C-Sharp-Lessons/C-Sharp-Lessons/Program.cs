68e36fd8e191f3a6f5ecae683c7d08647018803d
Edvin <edvin-hakobyan@mail.ru>
2018-12-03 19:18:13 +0400

    sds

--- C-Sharp-Lessons/C-Sharp-Lessons/Program.cs
+++ C-Sharp-Lessons/C-Sharp-Lessons/Program.cs
@@ -9,19 +9,6 @@ namespace C_Sharp_Lessons
 {
     class Program
     {
-        static int a;
-        static void Main(string[] args)
-        {
-            Class1 w = new Class1();
 
-            ThreadStart deligat = new ThreadStart(w.method);
-            Thread t = new Thread(deligat);
-            t.Start();
-        }
-
-        public static void chenge_a()
-        {
-            a++;
-        }
     }
 }
