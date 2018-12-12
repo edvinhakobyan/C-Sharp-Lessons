8e1e6544c84896f01e012b37bfce88ed1340faa3
edvinhakobyan <edvin1984edvin@gmail.com>
2018-12-12 15:14:22 +0400

    dfd

--- C-Sharp-Lessons/LINQ/Properties/AssemblyInfo.cs
+++ C-Sharp-Lessons/LINQ/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Polimorfizm")]
+[assembly: AssemblyTitle("LINQ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Polimorfizm")]
+[assembly: AssemblyProduct("LINQ")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c6c33228-fab3-4568-ac4d-1097364205fe")]
+[assembly: Guid("0d3ec068-7231-4cd1-b4a2-c80fcd13fa1f")]
 
 // Version information for an assembly consists of the following four values:
 //
