09e9812478c8f53a19952489e14c1df809d7b077
edvinhakobyan <edvin1984edvin@gmail.com>
2018-12-18 14:30:18 +0400

    www

---
+++ C-Sharp-Lessons/Dictionary/My_Dictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class My_Dictionary
+    {
+        Dictionary<string, string> dic;
+        
+        public My_Dictionary()
+        {
+            dic = new Dictionary<string, string>();
+
+        }
+
+
+
+
+        
+    }
+}
--- C-Sharp-Lessons/LINQ/Program.cs
+++ C-Sharp-Lessons/LINQ/Program.cs
@@ -10,7 +10,7 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
