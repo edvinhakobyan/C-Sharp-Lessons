65d97533e6ab46446eafa12574befcae1fafadeb
edvinhakobyan <edvin1984edvin@gmail.com>
2018-12-18 12:06:25 +0400

    www

--- C-Sharp-Lessons/LINQ/Program.cs
+++ C-Sharp-Lessons/LINQ/Program.cs
@@ -8,49 +8,9 @@ namespace LINQ
 {
     class Program
     {
-         static void Main(string[] args)
+        static void Main(string[] args)
         {
 
-            A rrr = new C();
-            Console.WriteLine(rrr.Print());
-            
-            
-            var list = from x in Enumerable.Range(2, 100)
-                       from y in Enumerable.Range(2, 100)
-                       let p = new { x, y, m = x * y }
-                       select p;
-
-
-
-            foreach (var item in list)
-            {
-                Console.WriteLine($"{item.x}  {item.y}  {item.m}  ");
-            }
         }
-
-
-
-
-        static bool IsPtime(int a)
-        {
-            for (int i = 2; i*i <= a; i++)
-                if (a % i == 0) return false;
-            return true;
-        }
-    }
-
-    public abstract class A
-    {
-        public virtual string Print() { return "A"; }
-    }
-
-    public class B : A
-    {
-        public virtual new string Print() { return "B"; }
-    }
-
-    public class C : B
-    {
-        public override string Print() { return "C"; }
     }
 }
