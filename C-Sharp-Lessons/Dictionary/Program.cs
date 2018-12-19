c7bf95523152fe6af39429e2d3ebb1dcb2481748
edvinhakobyan <edvin1984edvin@gmail.com>
2018-12-19 17:02:28 +0400

    add

--- C-Sharp-Lessons/Dictionary/Program.cs
+++ C-Sharp-Lessons/Dictionary/Program.cs
@@ -10,73 +10,74 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-            
-            Dictionary<string, string> dic = new Dictionary<string, string>();
-
-            dic.Add("a","ա"); 
-            dic.Add("b", "բ");
-            dic.Add("g", "գ");
-            dic.Add("d", "դ");
-            dic.Add("ye", "ե");
-            dic.Add("z", "զ");
-            dic.Add("e", "է");
-            dic.Add("@", "ը");
-            dic.Add("th", "թ");
-            dic.Add("j", "ժ");
-            dic.Add("i", "ի");
-            dic.Add("l", "լ");
-            dic.Add("kh", "խ");
-            dic.Add("tc", "ծ");
-            dic.Add("k", "կ");
-            dic.Add("h", "հ");
-            dic.Add("dz", "ձ");
-            dic.Add("x", "ղ");
-            dic.Add("tch", "ճ");
-            dic.Add("m", "մ");
-            dic.Add("y", "յ");
-            dic.Add("n", "ն");
-            dic.Add("sh", "շ");
-            dic.Add("vo", "ո");
-            dic.Add("ch", "չ");
-            dic.Add("p", "պ");
-            dic.Add("dj", "ջ");
-            dic.Add("ry", "ռ");
-            dic.Add("s", "ս");
-            dic.Add("v", "վ");
-            dic.Add("t", "տ");
-            dic.Add("r", "ր");
-            dic.Add("c", "ց");
-            dic.Add("u", "ու");
-            dic.Add("ph", "փ");
-            dic.Add("q", "ք");
-            dic.Add("yev", "և");
-            dic.Add("o", "օ");
-            dic.Add("f", "ֆ");
-
-
-            foreach(var item in dic.Keys)
-            {
-                Console.WriteLine($"{item} - {dic[item]}");
-            }
-
-
-
             Console.OutputEncoding = Encoding.UTF8;
-            string input = "";
+            Dictionary<char, char> dic = new Dictionary<char, char>();
+
+            dic.Add('a','ա'); 
+            dic.Add('b', 'բ');
+            dic.Add('g', 'գ');
+            dic.Add('d', 'դ');
+            dic.Add('e', 'ե');
+            dic.Add('z', 'զ');
+            dic.Add('1', 'է');
+            dic.Add('y', 'ը');
+            dic.Add('2', 'թ');
+            dic.Add('=', 'ժ');
+            dic.Add('i', 'ի');
+            dic.Add('l', 'լ');
+            dic.Add('[', 'խ');
+            dic.Add(']', 'ծ');
+            dic.Add('k', 'կ');
+            dic.Add('h', 'հ');
+            dic.Add('4', 'ձ');
+            dic.Add('x', 'ղ');
+            dic.Add('0', 'ճ');
+            dic.Add('m', 'մ');
+            dic.Add('j', 'յ');
+            dic.Add('n', 'ն');
+            dic.Add('\\', 'շ');
+            dic.Add('w', 'ո');
+            dic.Add('9', 'չ');
+            dic.Add('p', 'պ');
+            dic.Add('5', 'ջ');
+            dic.Add('r', 'ռ');
+            dic.Add('s', 'ս');
+            dic.Add('v', 'վ');
+            dic.Add('t', 'տ');
+            dic.Add('8', 'ր');
+            dic.Add('c', 'ց');
+            dic.Add('u', 'ւ');
+            dic.Add('3', 'փ');
+            dic.Add('q', 'ք');
+            dic.Add('7', 'և');
+            dic.Add('o', 'օ');
+            dic.Add('f', 'ֆ');
+
+            foreach (var item in dic.Keys)
+                Console.WriteLine($"{item} - {dic[item]}");
+            
+
+
             while (true)
             {
-
-
-                input += Console.ReadKey().KeyChar;
+                char input = Console.ReadKey().KeyChar;
+                if(input == '\r')
+                    Console.SetCursorPosition(0, Console.CursorTop + 1);
                 
+                char output;
+                if (Console.CursorLeft > 0)
+                {
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                    Console.Write(" ");
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                }
 
-
-
-
-
-
-
+                if (dic.TryGetValue(input, out output))
+                    Console.Write(output);
+                else
+                    Console.Write(input);
             }
+
         }
     }
 }
