957699d09b02bf9355ba257742f0bba7efc9edb6
Edvin <edvin-hakobyan@mail.ru>
2018-12-20 02:11:25 +0400

    add

---
+++ C-Sharp-Lessons/File/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintAllFiles(@"C:\");
+        }
+
+
+        public static void PrintAllFiles(string directory)
+        {
+            string[] File_Pats = Directory.GetFiles(directory);
+
+            foreach (string File_Pat in File_Pats)
+            {
+                Console.WriteLine(File_Pat);
+            }
+
+            string[] subdirectoryEntries = Directory.GetDirectories(directory);
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                PrintAllFiles(subdirectory);
+            }
+
+        }
+    }
+}
