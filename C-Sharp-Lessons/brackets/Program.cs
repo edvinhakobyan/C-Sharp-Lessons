62cc943ab6dd33b8a4310f0b69b62e45c7df84e0
Edvin <edvin-hakobyan@mail.ru>
2018-12-23 17:11:27 +0400

    add

---
+++ C-Sharp-Lessons/brackets/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsTrueBrackets(Console.ReadLine()).ToString());
+        }
+
+        private static bool IsTrueBrackets(string inp)
+        {
+            string list = "";
+            for (int i = 0; i < inp.Length; i++)
+            {
+                if (inp[i] == '(' || inp[i] == ')' || inp[i] == '{' || inp[i] == '}' ||
+                   inp[i] == '<' || inp[i] == '>' || inp[i] == '[' || inp[i] == ']')
+                { list += inp[i]; }
+            }
+                
+            while (true)
+            {
+                int count = 0;
+                if (list.Contains("()")) { list = list.Replace("()", ""); ++count; }
+                if (list.Contains("{}")) { list = list.Replace("{}", ""); ++count; }
+                if (list.Contains("<>")) { list = list.Replace("<>", ""); ++count; }
+                if (list.Contains("[]")) { list = list.Replace("[]", ""); ++count; }
+                if (count == 0) break;
+            }
+            return list.Length == 0;
+        }
+    }
+}
