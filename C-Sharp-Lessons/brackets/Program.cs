94bc583c09a9ebd192f5344ce94dcbf752fc64c9
edvinhakobyan <edvin1984edvin@gmail.com>
2018-12-24 12:03:59 +0400

    xc

--- C-Sharp-Lessons/brackets/Program.cs
+++ C-Sharp-Lessons/brackets/Program.cs
@@ -15,8 +15,7 @@ namespace brackets
 
         private static bool IsTrueBrackets(string inp)
         {
-            string list = "";
-            string brc = new string( inp.Where(t => (t == '(' || t == ')' || t == '{' || t == '}' ||
+            string list = new string( inp.Where(t => (t == '(' || t == ')' || t == '{' || t == '}' ||
                                                      t == '<' || t == '>' || t == '[' || t == ']')).ToArray());
 
             while (list.Contains("()") || list.Contains("{}") || list.Contains("<>") || list.Contains("[]"))
