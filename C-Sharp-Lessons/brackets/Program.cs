ef09ba99ae7906311e34c1d65f06c9e2821175c9
edvinhakobyan <edvin1984edvin@gmail.com>
2018-12-25 11:46:01 +0400

    ZZ

--- C-Sharp-Lessons/brackets/Program.cs
+++ C-Sharp-Lessons/brackets/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace brackets
@@ -10,13 +11,14 @@ namespace brackets
     {
         static void Main(string[] args)
         {
+            FloatingPointInParentheses(@"dfjdhfjdhf(0.2569)=34900385$%^{.6589}fgjk<2569>cvcvv[2365.]<5698.35698> ");
             Console.WriteLine(IsTrueBrackets(Console.ReadLine()).ToString());
         }
 
         private static bool IsTrueBrackets(string inp)
         {
-            inp = new string( inp.Where(t => (t == '(' || t == ')' || t == '{' || t == '}' ||
-                                              t == '<' || t == '>' || t == '[' || t == ']')).ToArray());
+            inp = new string(inp.Where(t => (t == '(' || t == ')' || t == '{' || t == '}' ||
+                                             t == '<' || t == '>' || t == '[' || t == ']')).ToArray());
 
             while (inp.Contains("()") || inp.Contains("{}") || inp.Contains("<>") || inp.Contains("[]"))
             {
@@ -25,5 +27,16 @@ namespace brackets
             }
             return inp.Length == 0;
         }
+
+        private static void FloatingPointInParentheses(string inp)
+        {
+            string pattern = @"[([{<]{1}[-+]{0,1}[0-9]{0,}[.]{0,1}[0-9]{0,}[)\]}>]{1}";
+            MatchCollection m = Regex.Matches(inp, pattern);
+            foreach (var item in m)
+                Console.WriteLine(item.ToString().Substring(1, item.ToString().Length - 2));
+        }
+
+
+
     }
 }
