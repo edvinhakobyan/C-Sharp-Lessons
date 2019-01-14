d785cef630b472a5768665ccc495f16d9db9f452
edvinhakobyan <edvin1984edvin@gmail.com>
2019-01-14 16:29:31 +0400

    sss

---
+++ C-Sharp-Lessons/Json/jsonopj.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    class jsonopj
+    {
+
+        public class Rootobject
+        {
+            public int[] array { get; set; }
+            public bool boolean { get; set; }
+            public string color { get; set; }
+            public object _null { get; set; }
+            public int number { get; set; }
+            public Object _object { get; set; }
+            public string _string { get; set; }
+        }
+
+        public class Object
+        {
+            public string a { get; set; }
+            public string c { get; set; }
+            public string e { get; set; }
+        }
+
+    }
+}
