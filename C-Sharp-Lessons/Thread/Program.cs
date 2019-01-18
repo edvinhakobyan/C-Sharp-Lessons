0d54335213976ca66fc761b22918fd7e120fb07d
edvinhakobyan <edvin1984edvin@gmail.com>
2019-01-18 18:24:59 +0400

    sdsd

---
+++ C-Sharp-Lessons/Thread/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_
+{
+    
+    class Program
+    {
+
+        static DateTime t = DateTime.Now;
+
+        static void secund_treade()
+        {
+            while (true)
+                Console.WriteLine("            " + DateTime.Now.Millisecond);
+        }
+
+        static void Main(string[] args)
+        {
+
+            ThreadStart start = new ThreadStart(secund_treade);
+
+            Thread tr = new Thread(start);
+
+            tr.Start();
+
+            while (true)
+                Console.WriteLine(DateTime.Now.Millisecond);
+
+
+
+        }
+    }
+}
