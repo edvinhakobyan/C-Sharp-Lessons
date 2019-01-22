dcf77f471a89cb8d8b1acce8c7d566b3d8263339
Edvin <edvin-hakobyan@mail.ru>
2019-01-22 11:38:33 -0800

    add bynary tree

---
+++ C-Sharp-Lessons/Bynay_Tree/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+
+        Tree tree = new Tree();
+        for (int i = 0; i < 100; i++)
+        {
+            Node temp = new Node(rand.Next(1000));
+            tree.Add_Node_Non_Recursiv(temp);
+        }
+        tree.Print(null);
+
+        Console.ReadKey();
+
+    }
+}
---
+++ C-Sharp-Lessons/Bynay_Tree/Tree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+class Tree
+{
+    Node Top;
+
+    public Tree()
+    {
+        Top = null;
+    }
+
+    public Tree(Node root)
+    {
+        Top = root;
+    }
+
+    public Tree(int rootValue)
+    {
+        Top = new Node(rootValue);
+    }
+
+    public void Add_Node_Non_Recursiv(Node _in)
+    {
+        if (Top == null)
+        {
+            Top = _in;
+            return;
+        }
+
+        Node Current = Top;
+        bool flag = false;
+
+        do
+        {
+            if (_in < Current)
+            {
+                if (Current.Left == null)
+                {
+                    Current.Left = _in;
+                    flag = true;
+                }
+                else
+                    Current = Current.Left;
+
+            }
+            if (_in >= Current)
+            {
+                if (Current.Right == null)
+                {
+                    Current.Right = _in;
+                    flag = true;
+                }
+                else
+                    Current = Current.Right;
+            }
+        } while (!flag);
+    }
+
+    public void Add_Node_Recursiv(int Value)
+    {
+
+
+    }
+
+    public void Print(Node nod)
+    {
+        if (nod == null)
+        {
+            nod = Top;
+        }
+        if (nod.Left == null)
+        {
+            Console.Write(nod.Value + " ");
+        }
+        else
+        {
+            Print(nod.Left);
+            Console.Write(nod.Value + " ");
+        }
+        if (nod.Right != null)
+        {
+            Print(nod.Right);
+        }
+    }
+}
